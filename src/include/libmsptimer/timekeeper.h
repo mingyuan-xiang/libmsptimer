608dbb2c75a98434cf6cc908824fcd15bd37c453
pouya <pouya@uchicago.edu>
2023-05-25 09:17:58 -0500

    update for abstract maker

--- src/include/libmsptimer/timekeeper.h
+++ src/include/libmsptimer/timekeeper.h
@@ -1,36 +1,36 @@
 #ifndef INCLUDE_TIMEKEEPER_H
 #define INCLUDE_TIMEKEEPER_H
+#include <libmsp/macro_basics.h>
 #include <stdint.h>
-
-// Adjust these if using a sleeper inside a stop watch while using smclk for stop watch.
-#define TIMEKEEPER_SLEEP LPM3;
-#define TIMEKEEPER_EXIT_SLEEP LPM3_EXIT;
 
 /* This library provides timekeeper functionlity as follows,
  *    stop_watch : measures how long a function takes using lfxt
  *                 accurate, but cannot be run intermittently
- *    time_out   : processes a timeout using the timeout circuitry.
- *                 for more info, refer to appropriate header.
  *    sleep_timer: sleeps for a certain duration
- *
- * Each module/functionality in this library uses a different timer
- * to prevent clashes. If using other libraries that use timers,
- * change the timers used below.
  */
+#ifndef CONFIG_SLEEP_TIMER
+#define CONFIG_SLEEP_TIMER 2
+#endif
 
-#define STOPWATCH_TIMER 1
-#define TIMEOUT_CALIB_TIMER 2
-#define SLEEP_TIMER 3
+#ifndef CONFIG_STOPWATCH_TIMER
+#define CONFIG_STOPWATCH_TIMER 1
+#endif
 
+#ifndef CONFIG_TIMEKEEPER_SLEEP
+#define CONFIG_TIMEKEEPER_SLEEP LPM3
+#define CONFIG_TIMEKEEPER_SLEEP_EXIT LPM3_EXIT
+#endif
+
+/* Function type for the stopwatch function. */
 typedef void (*stop_watch_fn)(void *);
 
 /* If using the timers in this library, initilize them as follows
  * Similarly, you can stop the initilized processes.
  *
- * Takes circa 35 ms based on my experiments.
+ * Takes circa 35 ms experiments, i.e. don't use this library in
+ * production.
  */
 void timers_init();
-
 void timers_stop();
 
 /* Measures how long a function takes with LFXTCLK.
@@ -41,36 +41,33 @@ void timers_stop();
  * lfxt runs the timer in with lfxtclk; if false, SMCLK is used
  */
 uint32_t stop_watch_cycle(stop_watch_fn fn, void *arg, bool aclk);
-
 uint32_t stop_watch_ms(stop_watch_fn fn, void *arg);
 
-
-/* Uses the SMCLK or ACLK (LFXTCLK ideally) to sleep for a certain duration in ms
- * Once the sleep is over, the function will return. ACLK & PJ.5-6 
- *    settings are altered in this function.
+/* Uses the SMCLK or ACLK (LFXTCLK ideally) to sleep for a certain duration in
+ * ms Once the sleep is over, the function will return. ACLK & PJ.5-6 settings
+ * are altered in this function.
  *
  * end is either in milli-seconds or cycles.
  * Note: if using SMCLK, don't use sleep_timer_ms. It DOES NOT work due to math.
  */
 void sleep_timer_cycle(uint32_t end, bool aclk);
-
 void sleep_timer_ms(uint32_t end);
 
 /* Measures how many cycles the smclk takes in 32768 of LFXT clock cycles.
  * To work needs timers init setup and smlk setup to desired clock.
- * 
+ *
  * In essence, this function runs a sleep timer with 32768 on the LFXT
  * and uses a stop watch with the smlk. So in reality, the value returned
- * is how long it took for the 32768 LFXT cycles to run + stopwatch DCO overhead.
- * With DCO set to 1 MHz, this takes 3 LFXT cycles, approximately 0.1 ms or 
- * 90-100 cycles.
- * 
+ * is how long it took for the 32768 LFXT cycles to run + stopwatch DCO
+ * overhead. With DCO set to 1 MHz, this takes 3 LFXT cycles, approximately 0.1
+ * ms or 90-100 cycles.
+ *
  */
 uint32_t measure_smlk_cycles();
 
 /* Measures how many cycles a clock approximately takes in 1 second.
- * To do so, it replaces the SMCLK with selected clk source and runs 
- * lfxt for 32768 cycles. Similar to above, it has some small overhead, 
+ * To do so, it replaces the SMCLK with selected clk source and runs
+ * lfxt for 32768 cycles. Similar to above, it has some small overhead,
  * about 0.1 ms or 90-100 DCO cycles on 1 MHz.
  * c goes as follows,
  *    1: DCO
@@ -79,7 +76,7 @@ uint32_t measure_smlk_cycles();
  *    4: VLOCLK
  *    5: LFXTCLK (calibrated based on this)
  *    6: HFXTCLK
- * 
+ *
  * Assumes lfxtclk is setup by timers_init
  */
 uint32_t measure_freq(uint8_t c);
--- src/timekeeper.c
+++ src/timekeeper.c
@@ -1,252 +1,225 @@
+#include <libmsp/clock.h>
+#include <libmsp/gpio.h>
+#include <libmsp/timer.h>
 #include <msp430.h>
-#include <clock.h>
-#include <timer.h>
-#include <gpio.h>
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-#include <timekeepers.h>
+#include <libmsptimer/timekeepers.h>
 
 static uint32_t stop_watch_tick = 0;
 static uint32_t sleep_timer_tick = 0;
 static uint16_t sleep_timer_last = 0;
 
 static uint16_t old_csctl2 = 0;
-void timers_init()
-{
-   lfxt_start(old_csctl2);
-}
+void timers_init() { lfxt_start(old_csctl2); }
 
-void timers_stop()
-{
-   lfxt_stop(old_csctl2);
-}
+void timers_stop() { lfxt_stop(old_csctl2); }
 
 // Measures how long a function takes.
-static inline uint32_t _stop_watch_(stop_watch_fn fn, void *arg, bool aclk)
-{
-   stop_watch_tick = 0;
+static inline uint32_t _stop_watch_(stop_watch_fn fn, void *arg, bool aclk) {
+  stop_watch_tick = 0;
 
-   if (aclk)
-   {
-      // Timer sourced by AUX and interrupt setup
-      timer_setup_cont(STOPWATCH_TIMER, ACLK, 1, 1);
-   }
-   else
-   {
-      timer_setup_cont(STOPWATCH_TIMER, SMCLK, 1, 1);
-   }
-   __enable_interrupt();
+  if (aclk) {
+    // Timer sourced by AUX and interrupt setup
+    timer_setup_cont(CONFIG_STOPWATCH_TIMER, ACLK, 1, 1);
+  } else {
+    timer_setup_cont(CONFIG_STOPWATCH_TIMER, SMCLK, 1, 1);
+  }
+  __enable_interrupt();
 
-   // start the timer on continuous mode & run
-   timer_start_cont(STOPWATCH_TIMER);
+  // start the timer on continuous mode & run
+  timer_start_cont(CONFIG_STOPWATCH_TIMER);
 
-   fn(arg);
+  fn(arg);
 
-   // disable the timer
-   timer_halt(STOPWATCH_TIMER);
+  // disable the timer
+  timer_halt(CONFIG_STOPWATCH_TIMER);
 
-   // read the last count
-   // this value is in lfxt clock cycles
-   uint32_t time;
+  // read the last count
+  // this value is in lfxt clock cycles
+  uint32_t time;
 
-   time = (stop_watch_tick << 16) | TA1R;
+  time = (stop_watch_tick << 16) | TA1R;
 
-   timer_halt(STOPWATCH_TIMER);
-   timer_reset(STOPWATCH_TIMER);
-   timer_IFG_disable(STOPWATCH_TIMER);
+  timer_halt(CONFIG_STOPWATCH_TIMER);
+  timer_reset(CONFIG_STOPWATCH_TIMER);
+  timer_IFG_disable(CONFIG_STOPWATCH_TIMER);
 
-   return time;
+  return time;
 }
 
-uint32_t stop_watch_cycle(stop_watch_fn fn, void *arg, bool aclk)
-{
-   return _stop_watch_(fn, arg, aclk);
+uint32_t stop_watch_cycle(stop_watch_fn fn, void *arg, bool aclk) {
+  return _stop_watch_(fn, arg, aclk);
 }
 
-uint32_t stop_watch_ms(stop_watch_fn fn, void *arg)
-{
-   uint32_t t = _stop_watch_(fn, arg, true);
+uint32_t stop_watch_ms(stop_watch_fn fn, void *arg) {
+  uint32_t t = _stop_watch_(fn, arg, true);
 
-   // turn into ms
-   t *= 1000;
-   t = t >> 15;
-   return t;
+  // turn into ms
+  t *= 1000;
+  t = t >> 15;
+  return t;
 }
 
-static inline void _sleep_timer_(bool aclk)
-{
-   if (sleep_timer_last == 0 && sleep_timer_tick == 0)
-      return;
+static inline void _sleep_timer_(bool aclk) {
+  if (sleep_timer_last == 0 && sleep_timer_tick == 0)
+    return;
 
-   // Timer sourced by ACLK and interrupt setup
-   __enable_interrupt();
+  // Timer sourced by ACLK and interrupt setup
+  __enable_interrupt();
 
-   if (sleep_timer_tick == 0)
-   {
-      timer_setup_up(SLEEP_TIMER, ACLK, 1, 1, sleep_timer_last);
-      timer_start_up(SLEEP_TIMER);
-   }
-   else
-   {
-      timer_setup_cont(SLEEP_TIMER, ACLK, 1, 1);
-      timer_start_cont(SLEEP_TIMER);
-   }
+  if (sleep_timer_tick == 0) {
+    timer_setup_up(CONFIG_SLEEP_TIMER, ACLK, 1, 1, sleep_timer_last);
+    timer_start_up(CONFIG_SLEEP_TIMER);
+  } else {
+    timer_setup_cont(CONFIG_SLEEP_TIMER, ACLK, 1, 1);
+    timer_start_cont(CONFIG_SLEEP_TIMER);
+  }
 
-   TIMEKEEPER_SLEEP;
+  CONFIG_TIMEKEEPER_SLEEP;
 
-   // _disable_interrupts();
-   timer_halt(SLEEP_TIMER);
-   timer_reset(SLEEP_TIMER);
-   timer_CCIE_disable(SLEEP_TIMER);
+  // _disable_interrupts();
+  timer_halt(CONFIG_SLEEP_TIMER);
+  timer_reset(CONFIG_SLEEP_TIMER);
+  timer_CCIE_disable(CONFIG_SLEEP_TIMER);
 
-   sleep_timer_last = 0;
-   sleep_timer_tick = 0;
+  sleep_timer_last = 0;
+  sleep_timer_tick = 0;
 }
 
-void sleep_timer_cycle(uint32_t end, bool aclk)
-{
-   sleep_timer_tick = end >> 16;
-   sleep_timer_last = end & 0xFFFF;
-   _sleep_timer_(aclk);
+void sleep_timer_cycle(uint32_t end, bool aclk) {
+  sleep_timer_tick = end >> 16;
+  sleep_timer_last = end & 0xFFFF;
+  _sleep_timer_(aclk);
 }
 
-void sleep_timer_ms(uint32_t end)
-{
-   // Turn end from ms into a lfxt clock cycles
+void sleep_timer_ms(uint32_t end) {
+  // Turn end from ms into a lfxt clock cycles
 
-   // each 2000 ms, the clock incurr one tick, soo..
-   sleep_timer_tick = end / 2000;
+  // each 2000 ms, the clock incurr one tick, soo..
+  sleep_timer_tick = end / 2000;
 
-   // // the rest should get translated into clock cycles
-   end %= 2000;
-   end = end << 15;
-   end /= 1000;
+  // // the rest should get translated into clock cycles
+  end %= 2000;
+  end = end << 15;
+  end /= 1000;
 
-   sleep_timer_last = end;
+  sleep_timer_last = end;
 
-   _sleep_timer_(true);
+  _sleep_timer_(true);
 }
 
-static void _smlk_cycles_kernel(void *x)
-{
-   sleep_timer_tick = 0;
-   sleep_timer_last = 32768;
-   _sleep_timer_(true);
+static void _smlk_cycles_kernel(void *x) {
+  sleep_timer_tick = 0;
+  sleep_timer_last = 32768;
+  _sleep_timer_(true);
 }
 
-uint32_t measure_smlk_cycles()
-{
-   return stop_watch_cycle(_smlk_cycles_kernel, 0, false);
+uint32_t measure_smlk_cycles() {
+  return stop_watch_cycle(_smlk_cycles_kernel, 0, false);
 }
 
-uint32_t measure_freq(uint8_t c)
-{
-   // This function is for diagnositcs mostly, so it's ok to have this 1 extra
-   // save function in there. It'll be fine.
+uint32_t measure_freq(uint8_t c) {
+  // This function is for diagnositcs mostly, so it's ok to have this 1 extra
+  // save function in there. It'll be fine.
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
-   uint16_t new, old;
-   uint32_t x = 0;
-   switch (c)
-   {
-   case 1:
-      clock_src_update(DCOCLK, DCOCLK, LFXTCLK, old);
-      x = stop_watch_cycle(_smlk_cycles_kernel, 0, false);
-      clock_src_update_all(old, new);
-      break;
-   case 2:
-      clock_src_update(DCOCLK, MODCLK, LFXTCLK, old);
-      x = stop_watch_cycle(_smlk_cycles_kernel, 0, false);
-      clock_src_update_all(old, new);
-      break;
-   case 3:
-      clock_src_update(DCOCLK, LFMODCLK, LFXTCLK, old);
-      x = stop_watch_cycle(_smlk_cycles_kernel, 0, false);
-      clock_src_update_all(old, new);
-      break;
-   case 4:
-      clock_src_update(DCOCLK, VLOCLK, LFXTCLK, old);
-      x = stop_watch_cycle(_smlk_cycles_kernel, 0, false);
-      clock_src_update_all(old, new);
-      break;
-   case 5:
-      x = stop_watch_cycle(_smlk_cycles_kernel, 0, true);
-      break;
-      break;
-   default:
-      break;
-   }
-   return x;
+  uint16_t new, old;
+  uint32_t x = 0;
+  switch (c) {
+  case 1:
+    clock_src_update(DCOCLK, DCOCLK, LFXTCLK, old);
+    x = stop_watch_cycle(_smlk_cycles_kernel, 0, false);
+    clock_src_update_all(old, new);
+    break;
+  case 2:
+    clock_src_update(DCOCLK, MODCLK, LFXTCLK, old);
+    x = stop_watch_cycle(_smlk_cycles_kernel, 0, false);
+    clock_src_update_all(old, new);
+    break;
+  case 3:
+    clock_src_update(DCOCLK, LFMODCLK, LFXTCLK, old);
+    x = stop_watch_cycle(_smlk_cycles_kernel, 0, false);
+    clock_src_update_all(old, new);
+    break;
+  case 4:
+    clock_src_update(DCOCLK, VLOCLK, LFXTCLK, old);
+    x = stop_watch_cycle(_smlk_cycles_kernel, 0, false);
+    clock_src_update_all(old, new);
+    break;
+  case 5:
+    x = stop_watch_cycle(_smlk_cycles_kernel, 0, true);
+    break;
+    break;
+  default:
+    break;
+  }
+  return x;
 }
 
-void __attribute__((interrupt(STIC3(TIMER, STOPWATCH_TIMER, _A1_VECTOR)))) STIC3(TIMER, STOPWATCH_TIMER, _A1_ISR)(void)
-{
-   switch (__even_in_range(STIC3(TA, STOPWATCH_TIMER, IV), TAIV__TAIFG))
-   {
-   case TAIV__NONE:
-      break; // No interrupt
-   case TAIV__TACCR1:
-      break; // CCR1 not used
-   case TAIV__TACCR2:
-      break; // CCR2 not used
-   case TAIV__TACCR3:
-      break; // reserved
-   case TAIV__TACCR4:
-      break; // reserved
-   case TAIV__TACCR5:
-      break; // reserved
-   case TAIV__TACCR6:
-      break;         // reserved
-   case TAIV__TAIFG: // overflow
-      stop_watch_tick++;
-      break;
-   default:
-      break;
-   }
+void
+    __attribute__((interrupt(STIC3(TIMER, CONFIG_STOPWATCH_TIMER, _A1_VECTOR))))
+    STIC3(TIMER, CONFIG_STOPWATCH_TIMER, _A1_ISR)(void) {
+  switch (__even_in_range(STIC3(TA, CONFIG_STOPWATCH_TIMER, IV), TAIV__TAIFG)) {
+  case TAIV__NONE:
+    break; // No interrupt
+  case TAIV__TACCR1:
+    break; // CCR1 not used
+  case TAIV__TACCR2:
+    break; // CCR2 not used
+  case TAIV__TACCR3:
+    break; // reserved
+  case TAIV__TACCR4:
+    break; // reserved
+  case TAIV__TACCR5:
+    break; // reserved
+  case TAIV__TACCR6:
+    break;          // reserved
+  case TAIV__TAIFG: // overflow
+    stop_watch_tick++;
+    break;
+  default:
+    break;
+  }
 }
 
-void __attribute__((interrupt(STIC3(TIMER, SLEEP_TIMER, _A1_VECTOR)))) STIC3(TIMER, SLEEP_TIMER, _A1_ISR)(void)
-{
-   switch (__even_in_range(STIC3(TA, SLEEP_TIMER, IV), TAIV__TAIFG))
-   {
-   case TAIV__NONE:
-      break; // No interrupt
-   case TAIV__TACCR1:
-      break; // CCR1 not used
-   case TAIV__TACCR2:
-      break; // CCR2 not used
-   case TAIV__TACCR3:
-      break; // reserved
-   case TAIV__TACCR4:
-      break; // reserved
-   case TAIV__TACCR5:
-      break; // reserved
-   case TAIV__TACCR6:
-      break;         // reserved
-   case TAIV__TAIFG: // overflow
-      // check to see if we need to switch from cont mode to up mode
-      if (--sleep_timer_tick == 0)
-      {
-         // time's up
-         if (sleep_timer_last == 0)
-         {
-            TIMEKEEPER_EXIT_SLEEP;
-         }
-         else
-         {
-            timer_IFG_disable(SLEEP_TIMER);
-            timer_setup_up(SLEEP_TIMER, ACLK, 1, 1, sleep_timer_last);
-            timer_start_up(SLEEP_TIMER);
-         }
+void __attribute__((interrupt(STIC3(TIMER, CONFIG_SLEEP_TIMER, _A1_VECTOR))))
+STIC3(TIMER, CONFIG_SLEEP_TIMER, _A1_ISR)(void) {
+  switch (__even_in_range(STIC3(TA, CONFIG_SLEEP_TIMER, IV), TAIV__TAIFG)) {
+  case TAIV__NONE:
+    break; // No interrupt
+  case TAIV__TACCR1:
+    break; // CCR1 not used
+  case TAIV__TACCR2:
+    break; // CCR2 not used
+  case TAIV__TACCR3:
+    break; // reserved
+  case TAIV__TACCR4:
+    break; // reserved
+  case TAIV__TACCR5:
+    break; // reserved
+  case TAIV__TACCR6:
+    break;          // reserved
+  case TAIV__TAIFG: // overflow
+    // check to see if we need to switch from cont mode to up mode
+    if (--sleep_timer_tick == 0) {
+      // time's up
+      if (sleep_timer_last == 0) {
+        CONFIG_TIMEKEEPER_SLEEP_EXIT;
+      } else {
+        timer_IFG_disable(CONFIG_SLEEP_TIMER);
+        timer_setup_up(CONFIG_SLEEP_TIMER, ACLK, 1, 1, sleep_timer_last);
+        timer_start_up(CONFIG_SLEEP_TIMER);
       }
-      break;
-   default:
-      break;
-   }
+    }
+    break;
+  default:
+    break;
+  }
 }
 
-void __attribute__((interrupt(STIC3(TIMER, SLEEP_TIMER, _A0_VECTOR)))) STIC3(TIMER, SLEEP_TIMER, _A0_ISR)(void)
-{
-   TIMEKEEPER_EXIT_SLEEP;
+void __attribute__((interrupt(STIC3(TIMER, CONFIG_SLEEP_TIMER, _A0_VECTOR))))
+STIC3(TIMER, CONFIG_SLEEP_TIMER, _A0_ISR)(void) {
+  CONFIG_TIMEKEEPER_SLEEP_EXIT;
 }
